3f807a97337773368989881e149be7d1775fb860
Mantas Sebeika <44549265+MantasSebeika@users.noreply.github.com>
2020-04-25 21:07:17 +0300

    admin and user get

--- Backend/server.js
+++ Backend/server.js
@@ -18,16 +18,6 @@ Klausimynas.run("CREATE TABLE IF NOT EXISTS vartotojai (id TEXT, imonespavadinim
 Klausimynas.run("CREATE TABLE IF NOT EXISTS klausimai (id TEXT, klausimas TEXT, kategorija TEXT, tipas TEXT)");
 Klausimynas.run("CREATE TABLE IF NOT EXISTS atsakymai (id TEXT, klausimoid TEXT, vartotojoid TEXT, atsakymas TEXT, komentarai TEXT)");
 
-app.post('/klausimai', function (req, res) {
-  var klausimas = req.body.klausimas;
-  var kategorija = req.body.kategorija;
-  var tipas = req.body.tipas;
-
-  Klausimynas.run(`INSERT INTO klausimai VALUES("${guid.v4()}", "${klausimas}", "${kategorija}", "${tipas}")`);
-
-  res.send("yes");
-});
-
 app.get('/klausimai', function (req, res) {
   Klausimynas.all("SELECT * from klausimai", [], (err, rows) => {
     if (err) {
@@ -40,12 +30,23 @@ app.get('/klausimai', function (req, res) {
 app.get('/dataimport', function (req, res) {
   const converter = csv({ delimiter: ";" }).fromFile('./Data.csv').then((json) => { json.forEach(item => { 
      Klausimynas.run(`INSERT INTO klausimai VALUES("${guid.v4()}", "${item['klausimas']}", "${item['kategorija']}", "random")`); 
-   // Klausimynas.run(`INSERT INTO klausimai VALUES("${guid.v4()}", "tu", "${item['kategorija']}", "${item['subkategorija']}", "random")`); 
   }); })
-  res.send("Testas")
+  res.send("Klausimai import")
 })
 
+app.get('/vartotojaiimport', function (req, res) {
+  const converter = csv({ delimiter: ";" }).fromFile('./vartotojai.csv').then((json) => { json.forEach(item => { 
+     Klausimynas.run(`INSERT INTO vartotojai VALUES("${guid.v4()}", "${item['imonespavadinimas']}", "${item['pastas']}", "${item['slaptazodis']}")`); 
+  }); })
+  res.send("Vartotojai import")
+})
 
+app.get('/administratoriaiimport', function (req, res) {
+  const converter = csv({ delimiter: ";" }).fromFile('./administratoriai.csv').then((json) => { json.forEach(item => { 
+     Klausimynas.run(`INSERT INTO vartotojai VALUES("${guid.v4()}", "${item['pastas']}", "${item['slaptazodis']}")`); 
+  }); })
+  res.send("Administratoriai import")
+})
 
 
 
